603c058dfebb3074f4d0ed7492dbc6af6e23f22c
Peter Yu <peter.yu@nexxspace.com>
2011-02-06 01:24:02 -0500

    added support for simple arithmetics.

--- tests/arithmetic_form_test.js
+++ tests/arithmetic_form_test.js
@@ -5,4 +5,16 @@ testCase('arithmetic_form_test', new function() {
 	    assertEquals(3, scm.eval('(+ 1 2)'));
 	};
 
+	this.substracting_two_numbers = function() {
+	    assertEquals(-1, scm.eval('(- 1 2)'));
+	};
+
+	this.multiplying_two_numbers = function() {
+	    assertEquals(6, scm.eval('(* 2 3)'));
+	};
+
+	this.dividing_two_numbers = function() {
+	    assertEquals(3, scm.eval('(/ 21 7)'));
+	};
+
     });
